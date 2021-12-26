473aa9d8ee8d0e66800c69a954523c49ec8d7063
Ducky <ducky@zio.sh>
2021-12-26 22:15:34 +0000

    scripts: fix spelling mistake

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,5 @@
 
 source "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.common.sh"
 
-echo "Generating repostory summary..."
+echo "Generating repository summary..."
 ostree summary --repo="$OSTREE_REPO_DIR" --update
