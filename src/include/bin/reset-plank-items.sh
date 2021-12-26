805580d16bca92fbb9d32db82cee6a18b3b2d7ab
Ducky <ducky@zio.sh>
2021-12-26 23:26:24 +0000

    bin: fix reset-plank-items not being executable

--- src/include/bin/reset-plank-items.sh
+++ src/include/bin/reset-plank-items.sh
@@ -11,4 +11,6 @@ fi
 rm -r $HOME_PATH/.config/plank/dock1/launchers
 cp -r /etc/skel/.config/plank/dock1/launchers $HOME_PATH/.config/plank/dock1/launchers
 dconf write /net/launchpad/plank/docks/dock1/dock-items "['gala-multitaskingview.dockitem', 'firefox.dockitem', 'microsoft-edge.dockitem', 'io.elementary.calendar.dockitem', 'io.elementary.music.dockitem', 'io.elementary.photos.dockitem', 'io.elementary.files.dockitem', 'org.gnome.Software.dockitem']"
-pkill plank # Pantheon will restart Plank after killing
+# TODO: Better way of restarting Plank
+# Pantheon will restart Plank after killing (but only once!)
+pkill plank
