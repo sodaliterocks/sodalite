26616d3bdf9ad25e713ca17e7bd69a6ebe5b44d9
Ducky <ducky@zio.sh>
2021-12-26 21:21:03 +0000

    scripts: various changes to build

--- build.sh
+++ build.sh
@@ -1,28 +1,4 @@
 #!/usr/bin/env bash
 
-if ! [ $(id -u) = 0 ]; then
-   echo "Must be ran as root!"
-   exit 1
-fi
-
-BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/build"
-if [ "$1" != "" ]; then
-   BASE_DIR=$1
-fi
-
-CACHE_DIR="$BASE_DIR/cache"
-REPO_DIR="$BASE_DIR/repo"
-VARIANT="fedora-sodalite"
-
-mkdir -p $CACHE_DIR
-mkdir -p $REPO_DIR
-
-if [ ! "$(ls -A $REPO_DIR)" ]; then
-   ostree --repo="${REPO_DIR}" init --mode=archive
-fi
-
-rpm-ostree compose tree \
-    --unified-core \
-    --cachedir="$CACHE_DIR" \
-    --repo="$REPO_DIR" \
-    src/${VARIANT}.yaml
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/scripts"
+$SCRIPT_DIR/build.sh
---
+++ scripts/.common.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if ! [ $(id -u) = 0 ]; then
+   echo "Must be ran as root!"
+   exit 1
+fi
+
+# TODO: Check ostree and rpm-ostree are installed
+
+BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+BUILD_DIR="$BASE_DIR/../build"
+if [ "$1" != "" ]; then
+   BUILD_DIR=$1
+fi
+
+OSTREE_CACHE_DIR="$BUILD_DIR/cache"
+OSTREE_REPO_DIR="$BUILD_DIR/repo"
