c6e3a589257b9769012951fdf36d31d6663fd382
Ducky <ducky@zio.sh>
2021-12-26 21:39:10 +0000

    scripts: fix path issues

--- scripts/.common.sh
+++ scripts/.common.sh
@@ -8,8 +8,8 @@ fi
 # TODO: Check ostree and rpm-ostree are installed
 # TODO: Make sure submodules we need for build are pulled in
 
-BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BUILD_DIR="$BASE_DIR/../build"
+BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
+BUILD_DIR="$BASE_DIR/build"
 if [ "$1" != "" ]; then
    BUILD_DIR=$1
 fi
