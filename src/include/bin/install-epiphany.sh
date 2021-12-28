7bc1a4bb99ca109b2a639058c207998fd5a43b1f
Ducky <ducky@zio.sh>
2021-12-28 11:52:07 +0000

    install-epiphany: fix not working as normal user

--- src/include/bin/install-epiphany.sh
+++ src/include/bin/install-epiphany.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+if ! [ $(id -u) = 0 ]; then
+   echo "Must be ran as root!"
+   exit 1
+fi
+
 echo "Installing Epiphany..."
 
 sodalite-install-appcenter-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub org.freedesktop.Platform.GL.default//21.08 --noninteractive
-flatpak install appcenter org.gnome.Epiphany//stable --noninteractive
+flatpak install --noninteractive --or-update --system flathub org.freedesktop.Platform.GL.default//21.08
+flatpak install --noninteractive --or-update --system appcenter org.gnome.Epiphany//stable
