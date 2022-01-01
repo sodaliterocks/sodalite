5d3d2717c858cf5eeca6c5caf601f56d0271ffb5
Ducky <ducky@zio.sh>
2022-01-01 04:10:19 +0000

    set-hostname (tools): add script to set/reset hostname

---
+++ src/include/tools/set-hostname.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [[ $(realpath -s "$0") == "/usr/bin/sodalite-"* ]]; then
+    . /usr/libexec/sodalite-common
+else
+    . "$(dirname "$(realpath -s "$0")")/common.sh"
+fi
+
+NEW_STATIC_HOSTNAME=$1
+NEW_PRETTY_HOSTNAME=$2
+
+test_root
+
+if { [[ -n $NEW_STATIC_HOSTNAME ]] && [[ $NEW_STATIC_HOSTNAME != "reset" ]]; }; then
+    echo "Setting hostname to '$NEW_STATIC_HOSTNAME'..."
+    hostnamectl hostname $NEW_STATIC_HOSTNAME
+
+    if [[ -n $NEW_PRETTY_HOSTNAME ]]; then
+        echo "Setting description to '$NEW_PRETTY_HOSTNAME'"
+        hostnamectl hostname "$NEW_PRETTY_HOSTNAME" --pretty
+    else
+        hostnamectl hostname "" --pretty
+    fi
+else
+    echo "Restting hostname..."
+    hostnamectl hostname ""
+    hostnamectl hostname "" --pretty
+    hostnamectl hostname "" --transient
+fi
