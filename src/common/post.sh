88a7d80c33e2f277020925f0f0eab5d8abc8c83b
Ducky <ducky@zio.sh>
2022-01-05 13:05:58 +0000

    [#2/#5] post: disable GDM service, enable LightDM service

--- src/common/post.sh
+++ src/common/post.sh
@@ -48,6 +48,8 @@ ln -s /usr/share/backgrounds/default/karsten-wurth-7BjhtdogU3A-unsplash.jpg /usr
 
 glib-compile-schemas /usr/share/glib-2.0/schemas
 
+systemctl disable gdm
+systemctl enable lightdm
 systemctl enable sodalite-generate-oem
 systemctl enable sodalite-install-appcenter-flatpak
 systemctl enable touchegg
