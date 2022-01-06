7dd8532581f578ce2299707b90e9ee0616ea9977
Ducky <ducky@zio.sh>
2022-01-06 02:42:21 +0000

    cleanup (tools): add ability to remove user cache

--- src/include/tools/cleanup.sh
+++ src/include/tools/cleanup.sh
@@ -18,8 +18,12 @@ function invoke_cleanup() {
     fi
 }
 
+real_user=$SUDO_USER
+real_user_dir=$(eval echo "~$real_user")
+
 test_root
 
 invoke_cleanup "rpm-ostree" "cleanup --pending --rollback" "Removing pending and rollback deployments"
 invoke_cleanup "rpm-ostree" "cleanup --base --repomd" "Removing cached and temporary data"
 invoke_cleanup "flatpak" "uninstall --unused" "Removing unused packages"
+invoke_cleanup "rm" "-rf $real_user_dir/.cache" "Removing user ($real_user) cache"
