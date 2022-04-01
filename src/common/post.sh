7c3e61d41bf9f5378b2a379764c0b13ab24cf15f
Ducky <ducky@sodalite.rocks>
2022-04-02 00:35:37 +0100

    🐞 workaround rpm directory change (fedora-coreos-tracker/issues#1139)

--- src/common/post.sh
+++ src/common/post.sh
@@ -56,33 +56,6 @@ else
     variant="unknown"
 fi
 
-#########
-# HACKS #
-#########
-
-# TODO: Work out if we even need these. These issues are pretty old.
-
-# BUG: https://github.com/projectatomic/rpm-ostree/issues/1542#issuecomment-419684977
-for x in /etc/yum.repos.d/*modular.repo; do
-    sed -i -e 's,enabled=[01],enabled=0,' ${x}
-done
-
-# BUG: https://bugzilla.redhat.com/show_bug.cgi?id=1265295
-if ! grep -q '^Storage=persistent' /etc/systemd/journald.conf; then
-    (cat /etc/systemd/journald.conf && echo 'Storage=persistent') > /etc/systemd.journald.conf.new
-    mv /etc/systemd.journald.conf{.new,}
-fi
-
-# SEE: https://src.fedoraproject.org/rpms/glibc/pull-request/4
-# Basically that program handles deleting old shared library directories
-# mid-transaction, which never applies to rpm-ostree. This is structured as a
-# loop/glob to avoid hardcoding (or trying to match) the architecture.
-for x in /usr/sbin/glibc_post_upgrade.*; do
-    if test -f ${x}; then
-        ln -srf /usr/bin/true ${x}
-    fi
-done
-
 ###################
 # OSTREE MUTATING #
 ###################
@@ -146,6 +119,38 @@ ln -s /usr/lib/sodalite-release /etc/sodalite-release
 ############
 # REMOVALS #
 ############
+
+#########
+# HACKS #
+#########
+
+# TODO: Work out if we even need these. These issues are pretty old.
+
+# BUG: https://github.com/projectatomic/rpm-ostree/issues/1542#issuecomment-419684977
+for x in /etc/yum.repos.d/*modular.repo; do
+    sed -i -e 's,enabled=[01],enabled=0,' ${x}
+done
+
+# BUG: https://bugzilla.redhat.com/show_bug.cgi?id=1265295
+if ! grep -q '^Storage=persistent' /etc/systemd/journald.conf; then
+    (cat /etc/systemd/journald.conf && echo 'Storage=persistent') > /etc/systemd.journald.conf.new
+    mv /etc/systemd.journald.conf{.new,}
+fi
+
+# SEE: https://src.fedoraproject.org/rpms/glibc/pull-request/4
+# Basically that program handles deleting old shared library directories
+# mid-transaction, which never applies to rpm-ostree. This is structured as a
+# loop/glob to avoid hardcoding (or trying to match) the architecture.
+for x in /usr/sbin/glibc_post_upgrade.*; do
+    if test -f ${x}; then
+        ln -srf /usr/bin/true ${x}
+    fi
+done
+
+# SEE: https://github.com/coreos/fedora-coreos-tracker/issues/1139
+if [[ $version_id -gt 35 ]]; then
+    rm -rf /usr/lib/sysimage/rpm
+fi
 
 # HACK: Removing files here instead because we're not using --unified-core (see https://github.com/sodaliterocks/sodalite/issues/9#issuecomment-1010384738)
 
