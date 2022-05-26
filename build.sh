cff6c4b045480fe6aabf36d7d46c0d70d8d22da9
Ducky <ducky@sodalite.rocks>
2022-05-26 04:14:54 +0100

    🐞 build: correct placement of quote

--- build.sh
+++ build.sh
@@ -74,7 +74,7 @@ echo "📄 Generating buildinfo file..."
 rpmostree_version="$(rpm-ostree --version)"
 
 buildinfo_file="$base_dir/src/sysroot/usr/lib/sodalite-buildinfo"
-buildinfo_content="\"BUILD_DATE=$(date +"%Y-%m-%d %T %z")\"
+buildinfo_content="BUILD_DATE=\"$(date +"%Y-%m-%d %T %z")\"
 \nBUILD_HOST_NAME=\"$(hostname -f)\"
 \nBUILD_HOST_OS=\"$(cat /usr/lib/os-release | grep "PRETTY_NAME" | sed "s/PRETTY_NAME=//" | tr -d '"')\"
 \nBUILD_HOST_KERNEL=\"$(uname -srp)\"
