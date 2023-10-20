9743af4a8221495357eea7d89fbb5e3dc3e9adc7
Ducky <ducky@sodalite.rocks>
2023-10-20 22:15:41 +0100

    🔧 80-tweaks-pantheon (scripts): add daemon-reload

--- src/scripts/80-tweaks-pantheon.sh
+++ src/scripts/80-tweaks-pantheon.sh
@@ -6,7 +6,10 @@ if [[ $_os_core == "pantheon" ]]; then
         mv /usr/bin/gnome-software-wrapper /usr/bin/gnome-software
     fi
 
+    systemctl daemon-reload
+
     systemctl disable gdm
+
     systemctl enable generate-oemconf
     systemctl enable lightdm
     systemctl enable touchegg
