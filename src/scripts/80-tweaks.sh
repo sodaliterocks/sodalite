389730b1c64ec139161e71dc94ee53975793f5ef
Ducky <ducky@sodalite.rocks>
2023-11-05 23:20:01 +0000

    🐞 80-tweaks (scripts): fix incorrect path

--- src/scripts/80-tweaks.sh
+++ src/scripts/80-tweaks.sh
@@ -2,7 +2,7 @@
 
 ln -s /usr/bin/rocks.sodalite.hacks /usr/bin/sodalite-hacks
 ln -s /usr/bin/firefox /usr/bin/rocks.sodalite.firefox
-ln -s /usr/share/fontconfig/conf.avail/63-inter.conf /etc/conf.d/63-inter.conf
+ln -s /usr/share/fontconfig/conf.avail/63-inter.conf /etc/fonts/conf.d/63-inter.conf
 
 glib-compile-schemas /usr/share/glib-2.0/schemas
 dconf update
