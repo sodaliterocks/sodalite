6be0d9cb90987abcd674b80bf86bd293f670b84d
Ducky <ducky@sodalite.rocks>
2024-07-21 23:51:16 +0100

    📦 various

--- src/scripts/50-file-removals.sh
+++ src/scripts/50-file-removals.sh
@@ -7,6 +7,7 @@ declare -a to_remove
 if [[ $_os_core == "gnome" ]]; then
     to_remove+=(
         # misc.
+        "/usr/share/backgrounds/gnome"
         "/usr/share/gnome-shell/extensions/apps-menu@gnome-shell-extensions.gcampax.github.com"
         "/usr/share/gnome-shell/extensions/launch-new-instance@gnome-shell-extensions.gcampax.github.com"
         "/usr/share/gnome-shell/extensions/places-menu@gnome-shell-extensions.gcampax.github.com"
@@ -144,6 +145,7 @@ fi
 if [[ -f "/usr/share/applications/rocks.sodalite.phone-mirror.desktop" ]]; then
     to_remove+=(
         "/usr/share/applications/scrcpy.desktop"
+        "/usr/share/applications/scrcpy-console.desktop"
     )
 fi
 
