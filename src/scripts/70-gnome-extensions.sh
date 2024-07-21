efaaf4df179ea1743a658a49d4cd33020e35166d
Ducky <ducky@sodalite.rocks>
2024-07-21 22:01:18 +0100

    🔧 gnome (cores): add dash-to-dock extension

--- src/scripts/70-gnome-extensions.sh
+++ src/scripts/70-gnome-extensions.sh
@@ -5,6 +5,7 @@ if [[ $_os_core == "gnome" ]]; then
 
     declare -a gnome_extensions=(
         "AlphabeticalAppGrid@stuarthayhurst"
+        "dash-to-dock@micxgx.gmail.com.zip"
     )
 
     for gnome_extension in ${gnome_extensions[@]}; do
