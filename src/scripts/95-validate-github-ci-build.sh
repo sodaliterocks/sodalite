eaf1e1fcc24e45e5d6a04d08d37aee0d9048ab65
Ducky <ducky@sodalite.rocks>
2024-07-27 17:08:58 +0100

    🐞 95-validate-github-ci-build: fix CI validation being incorrectly false

--- src/scripts/95-validate-github-ci-build.sh
+++ src/scripts/95-validate-github-ci-build.sh
@@ -4,7 +4,7 @@ if [[ $(get_buildopt "github-ci-build") == true ]]; then
     valid_ci_build=true
     ci_build_problems=()
 
-    [[ "$(get_property /usr/lib/os-release VENDOR)" != "sodaliterocks" ]] && ci_build_problems+="VENDOR (--vendor) not set to 'sodaliterocks'"
+    [[ "$(get_property /usr/lib/sodalite-buildinfo VENDOR)" != "sodaliterocks" ]] && ci_build_problems+="VENDOR (--vendor) not set to 'sodaliterocks'"
 
     for ci_build_problem in "${ci_build_problems[@]}"; do
         echo "   ⤷ $ci_build_problem"
