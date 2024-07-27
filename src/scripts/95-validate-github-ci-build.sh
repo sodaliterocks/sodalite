575211707d87e46d4f205e5a794dc2235b360256
Ducky <ducky@sodalite.rocks>
2024-07-27 16:44:13 +0100

    📦 various

--- src/scripts/95-validate-github-ci-build.sh
+++ src/scripts/95-validate-github-ci-build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ $(get_buildopt "github-ci-build") == true ]]; then
-    echo "Hello GitHub :)"
+    valid_ci_build=true
+    ci_build_problems=()
+
+    [[ "$(get_property /usr/lib/os-release VENDOR)" != "sodaliterocks" ]] && ci_build_problems+="VENDOR (--vendor) not set to 'sodaliterocks'"
+
+    for ci_build_problem in "${ci_build_problems[@]}"; do
+        echo "   ⤷ $ci_build_problem"
+        valid_ci_build=false
+    done
+
+    if [[ $valid_ci_build == false ]]; then
+        exit 255
+    fi
 fi
